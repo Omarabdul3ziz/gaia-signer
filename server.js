428294739176821f6e837c65c6f6af4cf6cd400c
Omar Abdulaziz <Omarabdulaziz@gmail.com>
2022-06-21 14:34:15 +0000

    fix the body object

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express();
 app.use(cors());
 
 app.use(express.json())
+app.use(express.urlencoded({extended: true})) // to read the req.body
 
 app.post('/sign', async (req, res) => {
 
